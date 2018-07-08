caff23b9ad804886ce6ea5808f632e9f6afdc8af
Parkle Lee <phparkle@gmail.com>
2018-07-08 08:16:34 +0800

    Add TestFixedArrayStack

---
+++ Source/FixedArrayStack.h
@@ -0,0 +1,50 @@
+// TODO Add description
+
+#pragma once
+
+template<class T>
+class FixedArrayStack {
+public:
+  // Creates a new stack with the given capacity
+  FixedArrayStack(int cap) {
+
+  }
+
+  // Destroys the stack
+  ~FixedArrayStack() {
+
+  }
+
+  // Creates a new stack with the given stack
+  FixedArrayStack(const FixedArrayStack& other) {
+
+  }
+
+  // Returns true if the stack is empty
+  bool empty() const {
+    return false;
+  }
+
+  // Returns the number of items in the stack
+  int size() const {
+    return 0;
+  }
+
+  // Adds an item to the top of the stack
+  void push(T item) {
+
+  }
+
+  // Removes the item at the top of the stack
+  void pop() {
+
+  }
+
+  // Returns the item at the top of the stack
+  T top() const {
+    return T();
+  }
+
+private:
+  // TODO Add data members
+};
